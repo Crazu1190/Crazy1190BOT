aa34821ed95d6c0dc781eb3f9d8db011865d6b4f
Crazu1190 <53394381+Crazu1190@users.noreply.github.com>
2019-08-09 17:29:20 +0200

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
 
-    bot.user.setActivity('Crazy1190BOT', {type: "STREAMING"});
+    bot.user.setActivity('Crazy1190BOT', {type: "WATCHING"});
 
     //bot.user.setGame('Nolify');
 });
