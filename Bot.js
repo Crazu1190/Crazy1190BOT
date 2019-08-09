a19f58845642f11ec921e214472b9e2f732a0909
Crazu1190 <53394381+Crazu1190@users.noreply.github.com>
2019-08-09 15:51:08 +0200

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === '!zbanuj') {
+    	message.reply('Poprawne użycie: !b @oznaczenie (powód)');
   	}
 });
 
