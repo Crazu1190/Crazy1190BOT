7c8dcd4615986e078bb46d1113612da2d359b0e2
Crazu1190 <53394381+Crazu1190@users.noreply.github.com>
2019-08-10 15:21:35 +0200

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -14,14 +14,5 @@ client.on('message', message => {
         }
 });
 
-client.on('message', msg => {
-    if (!msg.content.startsWith(process.env.PREFIX) || !msg.guild) return;
-    const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
-    const args = msg.content.split(' ').slice(1).join(' ');
-    if (command === 'guide') return msg.channel.send('https://discordapp.com');
-    else if (command === '!invite') return msg.channel.send(process.env.INVITE);
-});
-
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
