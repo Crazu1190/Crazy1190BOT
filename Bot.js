f29b4e3c5e91a16cc07b44af4e296c1673b68dcc
Crazu1190 <53394381+Crazu1190@users.noreply.github.com>
2019-08-10 11:42:17 +0200

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,8 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    client.user.setActivity('Przerwa techniczna!', {type: 'WATCHING'}); 
+client.on('ready', () => { 
     console.log('I am ready!');
 });
 
