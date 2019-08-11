ebe3cf484a065a569cd4545a8f19c99b324d79a4
Crazu1190 <53394381+Crazu1190@users.noreply.github.com>
2019-08-11 21:07:36 +0200

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -17,7 +17,7 @@ client.on('message', message => {
         }
     if (message.content === '!info {target}') {
         message.channel.send('Test');
-        message.channel.send('{target} Test pomyślny!');
+        message.channel.send('xd Test pomyślny!');
         }
 });
 
