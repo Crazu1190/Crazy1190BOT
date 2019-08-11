728ac06c48acf2733331fe24895fb2b72ed6dcc7
Crazu1190 <53394381+Crazu1190@users.noreply.github.com>
2019-08-11 21:11:45 +0200

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => { 
-    client.user.setActivity('nolify', {type: 'WATCHING'});
 });
 
 client.on('message', message => {
