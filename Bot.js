312c2e354df6832a8afb569cd582379096c4cd97
Crazu1190 <53394381+Crazu1190@users.noreply.github.com>
2019-08-12 13:52:17 +0200

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -15,7 +15,7 @@ client.on('message', message => {
     if (message.content === '!pomoc') {
         message.channel.send('ta komenda nie działa z powodu betatestów');
         }
-    if (message.content === '!info') {
+    if (message.content === '!info ${message.author}') {
         message.channel.send('Test');
         message.channel.send('${message.author} ,xd Test pomyślny!');
         }
