3c347544521a789f7c604cef5cb1ef10841b45a8
Crazu1190 <53394381+Crazu1190@users.noreply.github.com>
2020-05-21 10:31:44 +0200

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === '!eventy') {
+    if (message.content === '!rozpocznij') {
     	message.channel.send('Wyszukiwanie informacji...');
         message.channel.send(' ');
         message.channel.send(' ');
@@ -16,78 +16,11 @@ client.on('message', message => {
         message.delete('Wczytywanie...');
         message.reply('wszystko zostało wczytane!');
         message.channel.send('■■■■■■■■■■■■');
-        message.channel.send('**Najbilższy event:**');
-        message.channel.send('***Kruca***');
-        message.channel.send('**Data:** 30.08.2019 **Godz:** 11:30-12:00');
-        message.channel.send('**Brak dostępu do kanału głosowego**');
-        message.channel.send('**Obecny event: Event 2019** (Wymaga biletu)');
-        }
-    if (message.content === '<@&611150961418829846>') {
-        message.channel.send('Zostaw mnie! Chyba , że chcesz warnika😈');
-        }
-    if (message.content === '!info') {
-        message.channel.send('Wyszukiwanie informacji...');
-        message.channel.send(' ');
-        message.channel.send(' ');
-        message.channel.send(' ');
-        message.channel.send('Wczytywanie...');
-        message.channel.send(' ');
-        message.delete('Wczytywanie...')
-        message.reply('wszystko zostało wczytane!');
-        message.channel.send('■■■■■■■■■■■■');
-        message.channel.send('Serwer: NOLIFY®');
-        message.channel.send('Autor: Crazy1190');
-        message.channel.send('Język: Polski');
-        message.channel.send('Wersja: 1.0');
-        message.channel.send('■■■■■■■■■■■■');
-        }
-    if (message.content === '!killall') {
-        message.reply('strzela!');
-        message.channel.send('Możesz **nie zostać postrzelonym!**');
-        message.channel.send('**Używając komendę:**');
-        message.channel.send('**!oslona**');
-        message.channel.send('5');
-        message.channel.send('4');
-        message.channel.send('3');
-        message.channel.send('2');
-        message.channel.send('1');
-        message.channel.send('**Strzał!** przeżyli tylko ci co użyli osłony!');
-        }
-    if (message.content === '!partnerstwo') {
-        message.channel.send('**Partnerstwo wymagania:**');
-        message.delete('**Partnerstwo wymagania:**');
-        message.channel.send('50 osób i non toxic społeczność');
-        }
-    if (message.content === '!zglos') {
-        message.channel.send('**Administracja otworzy wkrótce ticketa!**');
-        message.channel.send('**Czas otwierania ticketa może potrwać do 24h**');
-        message.channel.send('**ID:** #09262');
-        message.channel.send('Możesz posiadać max 1 ticketa!');
-        message.channel.send('<@&600001366835855368>');
-        message.delete('<@&600001366835855368>');
-        message.reply('Twój ticket w: **OCZEKIWANIU**');
-        }
-    if (message.content === '8916') {
-        message.reply('udało ci się!');
-        message.channel.send('Wkrótce otrzymasz odznake!');
-        message.delete('Wkrótce otrzymasz odznake!');
-        }
-    if (message.content === '!klanbs') {
-        message.channel.send('Wygenerowano zaproszenie!');
-        message.channel.send('**Wymagania:**');
-        message.channel.send('200 pucharków i non **toxic**');
-        message.channel.send('**Klub jest zamknięty** podczas wysyłania prośby o dołączenie napisz swój nick');
-        message.channel.send('https://link.brawlstars.com/invite/band/pl?tag=LC08Y0J0&token=j66fnsdd');
-        }
-    if (message.content === '!chaejsjej') {
-        message.channel.send('■■■■■■■■■■■■');
-        message.channel.send('**Wersja:** 2.0 **(Beta)**');
-        message.channel.send('**Dodano:**');
-        message.channel.send('**!zglos** - Wysyła prośbe do administracji o utworzenie ticketa.');
-        message.channel.send('■■■■■■■■■■■■');
-        }
-    if (message.content === '!error') {
-        message.channel.send(':x: Error #386 wykryto poprzez AntiError.visualstudiovode wykryto podwójne wysyłanie wiadomości stopień errora: :x: Krytyczny :x: ');
+        message.channel.send('Witaj!');
+        message.channel.send('Bot został stworzony przez Michała G');
+        message.channel.send('Trwa konfiguracja...');
+        message.channel.send('Kończenie konfiguracji...');
+        message.channel.send('Wszystki przebiegło pomyślnie bot został skonfigurowany!');
         }
 });
 
